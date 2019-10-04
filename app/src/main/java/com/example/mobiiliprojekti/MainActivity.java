40bc27cc2af17c35ed87251106313b2a7bd2ed6f
Limenie <ninnikissanyaa@gmail.com>
2019-10-04 18:08:08 +0300

    Button
    Made a button for next activity

--- app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
+++ app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mobiiliprojekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button main2Activity = (Button) findViewById(R.id.button);
+        main2Activity.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View arg0){
+                Toast.makeText(getApplicationContext(), "Button selected !", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+    }});
     }
 }
