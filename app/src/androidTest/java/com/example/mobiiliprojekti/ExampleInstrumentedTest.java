cbad5fdfe7a81e897695e7e524ab56549cd38087
Limenie <ninnikissanyaa@gmail.com>
2019-10-04 14:25:19 +0300

    idk
    idk

--- app/src/androidTest/java/com/example/mobiiliprojekti/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mobiiliprojekti/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.mobiiliprojekti;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
