d5bb73c5b8d049b0711eedb6b3784355a470d981
Limenie <ninnikissanyaa@gmail.com>
2019-10-09 19:24:33 +0300

    Added Google Button
    It will open google on your webbrowser

--- app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
+++ app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mobiiliprojekti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Nappi vie main2Activity:lle joka aukaisee uuden sivun randomille eläin kuvalle
         Button main2Activity = (Button) findViewById(R.id.animalButton);
         main2Activity.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View arg0){
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
                 startActivity(intent);
             }});
+
+        // Nappi vie näkymään, jossa voidaan lisätä sivu menuun
         Button addSiteActivity = (Button) findViewById(R.id.addSiteButton);
         addSiteActivity.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View arg0){
@@ -29,5 +33,15 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), AddSiteActivity.class);
                 startActivity(intent);
             }});
+
+        // Nappi joka vie suoraan googleen käyttäjän
+        Button googleButton = (Button) findViewById(R.id.googleButton);
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent googleLinkki = new Intent(android.content.Intent.ACTION_VIEW);
+                googleLinkki.setData(Uri.parse("http://www.google.com"));
+                startActivity(googleLinkki);
+            }
+        });
     }
 }
