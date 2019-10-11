af5574b03753aff88c9b39c4c27c7df4116b2aca
Spop22 <suomipop22@hotmail.com>
2019-10-11 19:33:50 +0300

    Fixed add functionality and list size

--- app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
+++ app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mobiiliprojekti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,13 +17,18 @@ import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> list = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // Nappi vie main2Activity:lle joka aukaisee uuden sivun randomille eläin kuvalle
         Button main2Activity = (Button) findViewById(R.id.animalButton);
@@ -53,23 +59,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final ArrayList<String> list = new ArrayList<String>();
+        DataMan man = DataMan.getInstance();
+        list = man.getPeople();
 
         MyCustomAdapter adapter = new MyCustomAdapter(list, this); //instantiate custom adapter
         ListView listView = findViewById(R.id.myListView);
         listView.setAdapter(adapter); //handle listview and assign adapter
-
-
         
         Intent i = getIntent();
         String text = i.getStringExtra("Address");
 
-        // Defaulttina
-        list.add("google.com");
-        list.add("facebook.com");
-        list.add(text);
+
+
+
+
+        if(list.isEmpty()){
+            // Defaulttina
+            Log.d("MAIN","Lista ennen: "+ list);
+            list.add("google.com");
+            list.add("facebook.com");
+        }
+        if(text != null){
+            list.add(text);
+        }
+
+        Log.d("MAIN","Lista: "+ list);
         adapter.notifyDataSetChanged();
+        man.setPeople(list);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList("siteList", list);
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        list = savedInstanceState.getStringArrayList("siteList");
+    }
+}
 
+class DataMan {
+
+    private ArrayList<String> people = new ArrayList<String>();
+    private static DataMan instance;
+    private DataMan() {}
+
+    static DataMan getInstance() {
+        if( instance == null ) {
+            instance = new DataMan();
+        }
+        return instance;
+    }
+
+    public ArrayList<String> getPeople() {
+        return people;
+    }
+
+    public void setPeople(ArrayList<String> people) {
+        this.people = people;
     }
 }
