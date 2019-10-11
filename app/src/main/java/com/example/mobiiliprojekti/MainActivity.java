28ac6b4676d644747a4d3ff4ce871b272b7c23ee
Limenie <ninnikissanyaa@gmail.com>
2019-10-11 16:43:08 +0300

    Delete button for row
    Delete button in row so webpages can be deleted

--- app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
+++ app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
@@ -8,18 +8,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final ListView myListView = (ListView)findViewById(R.id.myListView);
-
 
         // Nappi vie main2Activity:lle joka aukaisee uuden sivun randomille eläin kuvalle
         Button main2Activity = (Button) findViewById(R.id.animalButton);
@@ -48,5 +49,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(googleLinkki);
             }
         });
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("item1");
+        list.add("item2");
+
+        //instantiate custom adapter
+        MyCustomAdapter adapter = new MyCustomAdapter(list, this);
+        //handle listview and assign adapter
+        ListView lView = (ListView)findViewById(R.id.myListView);
+        lView.setAdapter(adapter);
+
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/mobiiliprojekti/MyCustomAdapter.java
@@ -0,0 +1,69 @@
+package com.example.mobiiliprojekti;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyCustomAdapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<String>();
+    private Context context;
+    private TextView textView;
+
+    public MyCustomAdapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return 0;
+        //return list.get(pos).getId();
+        //just return 0 if your list items do not have an Id variable.
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_item, null);
+        }
+
+        //Handle TextView and display string from your list
+       textView = (TextView)view.findViewById(R.id.txtTitle);
+       textView.setText(list.get(position));
+
+        //Handle buttons and add onClickListeners
+        ImageButton deleteBtn = (ImageButton)view.findViewById(R.id.imgbtnDelete);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                list.remove(position); //or some other task
+                notifyDataSetChanged();
+            }
+        });
+
+        return view;
+    }
+}
+
